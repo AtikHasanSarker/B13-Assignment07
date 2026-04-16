8cc87de14cc1e252f14588c7ab65a44424e03d52
AtikHasanSarker <atiksagor24@gmail.com>
2026-04-16 22:37:24 +0600

    Change data fetch method and add loader

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -3,15 +3,34 @@ import { TimelineContext } from '../context/ContextProvider';
 import call from "../assets/icons/call.png";
 import text from "../assets/icons/text.png";
 import video from "../assets/icons/video.png";
+import { RiArrowDropDownLine } from 'react-icons/ri';
+import { GridLoader } from 'react-spinners';
 
 const Timeline = () => {
     const { timeline} = useContext(TimelineContext);
-    console.log(timeline)
     return (
       <div className="bg-gray-100">
         <div className="max-w-285 mx-auto px-5 py-10 md:py-20">
           <h3 className="font-semibold text-4xl md:text-5xl">Timeline</h3>
-          <div className="my-6">Filter Timeline</div>
+          <div className="dropdown dropdown-bottom">
+            <div tabIndex={0} role="button" className="btn m-1 ">
+              Filter Timeline <RiArrowDropDownLine className="text-3xl" />
+            </div>
+            <ul
+              tabIndex="-1"
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+            >
+              <li>
+                <a>Call</a>
+              </li>
+              <li>
+                <a>Text</a>
+              </li>
+              <li>
+                <a>Video</a>
+              </li>
+            </ul>
+          </div>
           <div className="space-y-6">
             {timeline.map((item, ind) => (
               <div
